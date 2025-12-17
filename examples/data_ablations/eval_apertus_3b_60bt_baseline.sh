d8c40b475fe4a75b80c94cef08645936ca5ed821
beme248 <beme248@users.noreply.github.com>
2025-12-17 14:04:55 +0100

    v0

---
+++ examples/data_ablations/eval_apertus_3b_60bt_baseline.sh
@@ -0,0 +1,16 @@
+
+export TOKENIZER="swiss-ai/Apertus-70B-2509"
+export BOS=true
+export WANDB_PROJECT="data-ablations-evals"
+export MEGATRON_BRANCH="81d44dbe25c22190989fa8a12284bdb8d1303b40"
+export TRANSFORMERS_BRANCH=v4.50.0+swissai
+
+CKPT_PATH="/iopsstor/scratch/cscs/bmessmer/data_ablations/Megatron-LM/logs/Meg-Runs/data-ablations/apertus3-3b-64-nodes-phase-4-baseline/checkpoints/"
+
+ITS=(0030000)
+TOK_PER_IT=$(( 512 * 4096 ))
+MODEL=apertus3-3b-64-nodes-phase-4-baseline
+
+for IT in "${ITS[@]}"; do
+	sbatch --job-name eval-$MODEL-$IT scripts/evaluate.sbatch $CKPT_PATH $IT $TOK_PER_IT $MODEL
+done
